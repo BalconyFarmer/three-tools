a1a34ea3520690b099fd3053fdb4a6b7f037cae5
wangkewei <1067647932@qq.com>
2024-07-29 09:35:22 +0800

    11

--- src/main.js
+++ src/main.js
@@ -1,13 +1,95 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// https://juejin.im/post/6844903937745616910
 import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
+import App from './App'
 import router from './router'
-import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import axios from 'axios';
+import Vuex from 'vuex'
+import cookies from 'vue-cookies'
+import VuexPersistence from "vuex-persist";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueAMap from "vue-amap";
 
 Vue.config.productionTip = false
+Vue.use(Antd)
+Vue.use(Vuex)
+Vue.prototype.$axios = axios
+Vue.use(cookies)
+Vue.use(ElementUI);
+Vue.use(VueAMap);
 
+// 先创建一个对象并进行配置
+const vuexPersist = new VuexPersistence({
+    strictMode: true,
+    storage: window.localStorage, // 存入localStorage
+    reducer: state => ({
+        count: state.count,       // 这个就是存入localStorage的值
+        userInf: state.userInf,
+    })
+});
+
+const store = new Vuex.Store({
+    state: {
+        count: 0,
+        userInf: {},
+        list: [1, 2, 3, 4, 5, 6, 7, 8]
+    },
+    mutations: {
+        increment(state) {
+            state.count++
+        },
+        // 保存用户信息
+        setUserInf(state, userInf) {
+            state.userInf = userInf
+        },
+        // 删除用户信息
+        clearUserInf(state) {
+            state.userInf = null
+        },
+        ///
+        RESTORE_MUTATION: vuexPersist.RESTORE_MUTATION, // this mutation **MUST** be named "RESTORE_MUTATION"
+        setQuery(state, query) {
+            state.query = query;
+        }
+
+    },
+    getters: { //  这个主要是对状态的处理，相当于把状态处理的方法抽成公共部分来管理了
+        modifyArr(state) { // 一般化getter
+            return state.list.filter((item, index, arr) => {
+                return item % 2 == 0;
+            })
+        },
+        getLength(state, getter) { // 方法里面传getter，调用modifyArr来计算长度
+            return getter.modifyArr.length;
+        }
+    },
+
+    plugins: [vuexPersist.plugin]
+})
+
+/* eslint-disable no-new */
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    el: '#app',
+    router,
+    store,
+    cookies,
+    components: {App},
+    template: '<App/>'
+})
+
+VueAMap.initAMapApiLoader({
+    key: 'b45d59c7d49f2c8d460c55f6e90f2d22',
+    plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor', 'AMap.DistrictLayer', 'AMap.DistrictSearch', 'AMap.Lights'],
+    // 默认高德 sdk 版本为 1.4.4
+    v: '1.4.4'
+});
+
+window.addEventListener('load', function () {
+
+
+})
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,35 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Router from 'vue-router'
+import HomePage from '@/components/HomePage'
+import ExperimentArea from "../components/ExperimentArea/ExperimentArea";
+import DigitalCity from "../components/DigitalCity/DigitalCity";
+import MenuD3 from "../components/Menu3d/MenuD3";
+import MenuD31 from "../components/Menu3d2/Menu3d2"
+import RotateImg from "../components/Menu3d2/RotateImg";
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+const router = new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'HomePage',
+            component: HomePage,
+            children: [
+                {path: 'goDigitalCity', component: DigitalCity},
+                {path: 'experimentArea', component: ExperimentArea, children: []},
+                {path: 'go3dMenu', component: MenuD3, children: []},
+                {path: 'go3dMenu1', component: MenuD31, children: []},
+                {path: 'rotateImg', component: RotateImg, children: []},
+            ]
+        }
+    ],
+    mode: "history", // hash
+    base: process.env.NODE_ENV === "development" ? "/test" : "/Three3D",
 })
 
+router.beforeEach((to, from, next) => {
+    next()
+    localStorage.setItem('currentRouter', to.path)
+})
 export default router
