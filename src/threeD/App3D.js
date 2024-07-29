1b1bbc2785f7f2a2dee23c2307ab41f0b99ca883
wangkewei <1067647932@qq.com>
2024-07-29 14:43:43 +0800

    11

--- src/threeD/App3D.js
+++ src/threeD/App3D.js
@@ -1,45 +1,44 @@
 import * as THREE from "three";
-import { SceneInitializer } from "./SceneInitializer"; // 导入 SceneInitializer 类
-
-import { RaycasterHelper } from "@/threeD/interaction/RaycasterHelper";
-import { ImportObjs } from "@/threeD/loaders/ImportObjs";
-import { ImportFBX } from '@/threeD/animation/ImportFBX';
-import { EventCube } from '@/threeD/interaction/EventCube';
-import { JudgeFace3 } from '@/threeD/basicMath/JudgeFace3';
-import { FlowPipes } from '@/threeD/animation/FlowPipe/FlowPipes';
-import { Helper } from '@/threeD/helpers/Helper';
-import { Controls } from '@/threeD/sceneBasic/Controls';
-import { ExportImport } from '@/threeD/loaders/ExportImport';
-import { Car } from '@/threeD/PROJECTS/game/Car';
-import { PhysiSimulate } from '@/threeD/PROJECTS/game/PhysiSimulate';
-import { MakeBufferGeometryMesh } from './basicMesh/MakeBufferGeometryMesh';
-import { MakeBufferGeometryCube } from './basicMesh/MakeBufferGeometryCube';
-import { MakeGeometryMesh } from './basicMesh/MakeGeometryMesh';
-import { MakeMeshPoint } from './basicMesh/MakeMeshPoint';
-import { MakeMeshLine } from './basicMesh/MakeMeshLine';
-import { TransformMesh } from './interaction/TransformMesh';
-import { TakePoint } from './interaction/TakePoint';
-import { Animation } from './animation/Animation';
-import { Sound } from './interaction/Sound';
-import { ArrowLine } from './helpers/representationalviewer/ArrowLine';
-import { Matrix4Practice } from './basicMath/Matrix4Practice';
-import { EulerPractice } from './basicMath/EulerPractice';
-import { QuaternionPractice } from './basicMath/QuaternionPractice';
-import { OutlineShineEffect } from './interaction/OutlineShineEffect';
-import { PlayVideo } from './materials/PlayVideo';
-import { TextureCanvasAnimation } from './materials/TextureCanvasAnimation';
-import { BasicMaterials } from '@/threeD/materials/BasicMaterials';
-import { GUIApp } from './GUI/GUIApp';
-import { ShadowLight } from './undefinedNow/ShadowLight';
-import { LittleWindow } from './helpers/representationalviewer/LittleWindow';
-import { MultipleElements } from './undefinedNow/MultipleElements';
-import { AdvancedMaterial } from './materials/AdvancedMaterial';
-import { SkyBox } from './sceneBasic/SkyBox';
-import { WaterPlane } from './materials/WaterPlane';
-import { LightningStrike } from './LightningStrike';
-import { BloomOnly } from "./Bloom/BloomOnly";
-import { Grass } from "./Grass";
-import { Cloud } from "./Cloud";
+import {SceneInitializer} from "./SceneInitializer"; // 导入 SceneInitializer 类
+import {RaycasterHelper} from "@/threeD/interaction/RaycasterHelper";
+import {ImportObjs} from "@/threeD/loaders/ImportObjs";
+import {ImportFBX} from '@/threeD/animation/ImportFBX';
+import {EventCube} from '@/threeD/interaction/EventCube';
+import {JudgeFace3} from '@/threeD/basicMath/JudgeFace3';
+import {FlowPipes} from '@/threeD/animation/FlowPipe/FlowPipes';
+import {Helper} from '@/threeD/helpers/Helper';
+import {Controls} from '@/threeD/sceneBasic/Controls';
+import {ExportImport} from '@/threeD/loaders/ExportImport';
+import {Car} from '@/threeD/PROJECTS/game/Car';
+import {PhysiSimulate} from '@/threeD/PROJECTS/game/PhysiSimulate';
+import {MakeBufferGeometryMesh} from './basicMesh/MakeBufferGeometryMesh';
+import {MakeBufferGeometryCube} from './basicMesh/MakeBufferGeometryCube';
+import {MakeGeometryMesh} from './basicMesh/MakeGeometryMesh';
+import {MakeMeshPoint} from './basicMesh/MakeMeshPoint';
+import {MakeMeshLine} from './basicMesh/MakeMeshLine';
+import {TransformMesh} from './interaction/TransformMesh';
+import {TakePoint} from './interaction/TakePoint';
+import {Animation} from './animation/Animation';
+import {Sound} from './interaction/Sound';
+import {ArrowLine} from './helpers/representationalviewer/ArrowLine';
+import {Matrix4Practice} from './basicMath/Matrix4Practice';
+import {EulerPractice} from './basicMath/EulerPractice';
+import {QuaternionPractice} from './basicMath/QuaternionPractice';
+import {OutlineShineEffect} from './interaction/OutlineShineEffect';
+import {PlayVideo} from './materials/PlayVideo';
+import {TextureCanvasAnimation} from './materials/TextureCanvasAnimation';
+import {BasicMaterials} from '@/threeD/materials/BasicMaterials';
+import {GUIApp} from './GUI/GUIApp';
+import {ShadowLight} from './undefinedNow/ShadowLight';
+import {LittleWindow} from './helpers/representationalviewer/LittleWindow';
+import {MultipleElements} from './undefinedNow/MultipleElements';
+import {AdvancedMaterial} from './materials/AdvancedMaterial';
+import {SkyBox} from './sceneBasic/SkyBox';
+import {WaterPlane} from './materials/WaterPlane';
+import {LightningStrike} from './LightningStrike';
+import {BloomOnly} from "./Bloom/BloomOnly";
+import {Grass} from "./Grass";
+import {Cloud} from "./Cloud";
 
 export default class App3D {
     constructor(dom) {
@@ -104,80 +103,7 @@ export default class App3D {
     init() {
         const sceneInitializer = new SceneInitializer(this, this.dom);
         sceneInitializer.init();
-
         this.initializeComponents();
     }
 
-    addCustomCube() {
-        this.makeBufferGeometryCube = new MakeBufferGeometryCube(this);
-    }
-
-    getSceneChildren() {
-        const result = [];
-
-        const recurrenceScene = (aim, origin) => {
-            origin.forEach(item => {
-                const data = {
-                    title: item.cname || item.type,
-                    key: item.uuid,
-                    children: item.children.length > 0 ? [] : null
-                };
-                aim.push(data);
-                if (item.children.length > 0) {
-                    recurrenceScene(data.children, item.children);
-                }
-            });
-        };
-
-        recurrenceScene(result, this.scene.children);
-        return result;
-    }
-
-    getMeshByUUID(uuid) {
-        const mesh = this.scene.getObjectByProperty('uuid', uuid[0]);
-        if (mesh) {
-            console.log('根据UUID获取', mesh);
-            if (this.boxHelper) {
-                this.scene.remove(this.boxHelper);
-            }
-            this.boxHelper = new THREE.BoxHelper(mesh, 0xffff00);
-            this.boxHelper.cname = '选择网格辅助';
-            this.scene.add(this.boxHelper);
-            return mesh;
-        }
-    }
-
-    getMeshByUUIDDispose() {
-        if (this.boxHelper) {
-            this.scene.remove(this.boxHelper);
-        }
-    }
-
-    removeFromQueue(name) {
-        this.renderQueue = this.renderQueue.filter(item => item.name !== name);
-    }
-
-    /**
-     * 更新相机
-     */
-    onRender() {
-        this.renderer.render(this.scene, this.camera); //执行渲染操作
-    }
-
-    /**
-     * 动态调整屏幕大小
-     */
-    windowRelise() {
-        const { innerWidth: width, innerHeight: height } = window;
-        this.dom.width = width;
-        this.dom.height = height;
-        this.renderer.setSize(width, height);
-        this.camera.aspect = width / height;
-        this.camera.updateProjectionMatrix();
-    }
-
-    destroy() {
-        this.loopFlag = false;
-        this.transformMesh.removeEvent();
-    }
 }
--- src/threeD/SceneInitializer.js
+++ src/threeD/SceneInitializer.js
@@ -17,7 +17,7 @@ export class SceneInitializer {
     }
 
     initCamera() {
-        const { width, height } = this.dom;
+        const {width, height} = this.dom;
         const aspectRatio = width / height;
         this.camera = new THREE.PerspectiveCamera(30, aspectRatio, 1, 10000);
         this.camera.position.set(300, 300, 300);
@@ -31,14 +31,9 @@ export class SceneInitializer {
         const ambient = new THREE.AmbientLight(0x444444, intensity);
         this.scene.add(ambient);
 
-        const pointLightPositions = [
-            { x: distance, y: 0, z: 0 },
-            { x: -distance, y: 0, z: 0 },
-            { x: 0, y: distance, z: 0 },
-            { x: 0, y: -distance, z: 0 },
-            { x: 0, y: 0, z: distance },
-            { x: 0, y: 0, z: -distance }
-        ];
+        const pointLightPositions = [{x: distance, y: 0, z: 0}, {x: -distance, y: 0, z: 0}, {
+            x: 0, y: distance, z: 0
+        }, {x: 0, y: -distance, z: 0}, {x: 0, y: 0, z: distance}, {x: 0, y: 0, z: -distance}];
 
         pointLightPositions.forEach((pos, index) => {
             const pointLight = new THREE.PointLight(0xffffff, intensity);
@@ -54,8 +49,8 @@ export class SceneInitializer {
     }
 
     initRenderer() {
-        const { width, height } = this.dom;
-        this.renderer = new THREE.WebGLRenderer({ canvas: this.dom, antialias: true });
+        const {width, height} = this.dom;
+        this.renderer = new THREE.WebGLRenderer({canvas: this.dom, antialias: true});
         this.renderer.setSize(width, height);
         this.renderer.setClearColor(0x000000, 0.1);
         this.renderer.shadowMap.enabled = true;
@@ -66,13 +61,22 @@ export class SceneInitializer {
         const run = () => {
             if (this.app.loopFlag) {
                 requestAnimationFrame(run);
-                this.app.onRender();
+                this.onRender();
                 this.app.renderQueue.forEach(item => item());
+                this.app.eventBus.dispatchEvent({type: 'updateLeftTreeData', message: 'message!'});
             }
         };
 
         run();
     }
+
+    /**
+     * 更新相机
+     */
+    onRender() {
+        this.renderer.render(this.scene, this.camera); //执行渲染操作
+    }
+
 
     init() {
         this.initScene();
@@ -80,5 +84,73 @@ export class SceneInitializer {
         this.initLight();
         this.initRenderer();
         this.startLoop();
+        this.app.getMeshByUUIDDispose = this.getMeshByUUIDDispose
+        this.app.getSceneChildren = this.getSceneChildren
+        this.app.getMeshByUUID = this.getMeshByUUID
+        this.app.removeFromQueue = this.removeFromQueue
+        this.app.windowRelise = this.windowRelise
+        this.app.destroy = this.destroy
+    }
+
+
+    /**
+     * 动态调整屏幕大小
+     */
+    windowRelise() {
+        const {innerWidth: width, innerHeight: height} = window;
+        this.dom.width = width;
+        this.dom.height = height;
+        this.renderer.setSize(width, height);
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+    }
+
+    destroy() {
+        this.loopFlag = false;
+        this.transformMesh.removeEvent();
+    }
+
+
+    removeFromQueue(name) {
+        this.renderQueue = this.renderQueue.filter(item => item.name !== name);
+    }
+
+    getMeshByUUIDDispose() {
+        if (this.boxHelper) {
+            this.scene.remove(this.boxHelper);
+        }
+    }
+
+    getSceneChildren() {
+        const result = [];
+
+        const recurrenceScene = (aim, origin) => {
+            origin.forEach(item => {
+                const data = {
+                    title: item.cname || item.type, key: item.uuid, children: item.children.length > 0 ? [] : null
+                };
+                aim.push(data);
+                if (item.children.length > 0) {
+                    recurrenceScene(data.children, item.children);
+                }
+            });
+        };
+
+        recurrenceScene(result, this.scene.children);
+        return result;
+    }
+
+    getMeshByUUID(uuid) {
+        const mesh = this.scene.getObjectByProperty('uuid', uuid[0]);
+        if (mesh) {
+            console.log('根据UUID获取', mesh);
+            if (this.boxHelper) {
+                this.scene.remove(this.boxHelper);
+            }
+            this.boxHelper = new THREE.BoxHelper(mesh, 0xffff00);
+            this.boxHelper.cname = '选择网格辅助';
+            this.scene.add(this.boxHelper);
+            return mesh;
+        }
     }
 }
